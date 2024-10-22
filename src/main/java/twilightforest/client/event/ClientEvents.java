d527927525932f32fb8264a914d5609c20b08e8c
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-10-22 10:59:24 -0600

    setup gradle for 1.21.2

--- src/main/java/twilightforest/client/event/ClientEvents.java
+++ src/main/java/twilightforest/client/event/ClientEvents.java
@@ -18,7 +18,6 @@ import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.Style;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraft.sounds.Music;
 import net.minecraft.sounds.Musics;
