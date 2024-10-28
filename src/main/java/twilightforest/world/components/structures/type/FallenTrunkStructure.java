7ee34f912bc7e6d968779e627d44e1b72ab3fa74
Sleepy-Horse <andrei.kunitsin@gmail.com>
2024-10-28 12:29:23 +0100

    Register FallenTrunkStructure

---
+++ src/main/java/twilightforest/world/components/structures/type/FallenTrunkStructure.java
@@ -0,0 +1,76 @@
+package twilightforest.world.components.structures.type;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.RegistryFixedCodec;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.random.WeightedRandomList;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureSpawnOverride;
+import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraft.world.level.levelgen.structure.TerrainAdjustment;
+import net.minecraft.world.level.storage.loot.LootTable;
+import twilightforest.init.TFBlocks;
+import twilightforest.init.TFEntities;
+import twilightforest.init.TFStructureTypes;
+import twilightforest.loot.TFLootTables;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class FallenTrunkStructure extends Structure {
+	public static final MapCodec<FallenTrunkStructure> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+		Structure.settingsCodec(instance),
+		IntProvider.codec(8, 32).fieldOf("length").forGetter(s -> s.length),
+		BlockStateProvider.CODEC.fieldOf("log").forGetter(s -> s.log),
+		ResourceKey.codec(Registries.LOOT_TABLE).fieldOf("chest_loot_table").forGetter(s -> s.chestLootTable),
+		RegistryFixedCodec.create(Registries.ENTITY_TYPE).fieldOf("spawner_monster").forGetter(s -> s.spawnerMonster)
+	).apply(instance, FallenTrunkStructure::new));
+
+	private final IntProvider length;
+	private final BlockStateProvider log;
+	private final ResourceKey<LootTable> chestLootTable;
+	private final Holder<EntityType<?>> spawnerMonster;
+
+	protected FallenTrunkStructure(StructureSettings settings, IntProvider length, BlockStateProvider log, ResourceKey<LootTable> chestLootTable, Holder<EntityType<?>> spawnerMonster) {
+		super(settings);
+		this.length = length;
+		this.log = log;
+		this.chestLootTable = chestLootTable;
+		this.spawnerMonster = spawnerMonster;
+	}
+
+	@Override
+	public Optional<GenerationStub> findGenerationPoint(GenerationContext context) {
+		return onTopOfChunkCenter(context, Heightmap.Types.WORLD_SURFACE_WG, p_227598_ -> this.findGenerationPoint(context));
+	}
+
+	@Override
+	public StructureType<?> type() {
+		return TFStructureTypes.FALLEN_TRUNK.get();
+	}
+
+	public static FallenTrunkStructure buildStructureConfig(HolderSet<Biome> biomes) {
+		return new FallenTrunkStructure(
+			new Structure.StructureSettings(
+				biomes,
+				Arrays.stream(MobCategory.values()).collect(Collectors.toMap(category -> category, category -> new StructureSpawnOverride(StructureSpawnOverride.BoundingBoxType.STRUCTURE, WeightedRandomList.create()))), // Landmarks have Controlled Mob spawning
+				GenerationStep.Decoration.SURFACE_STRUCTURES,
+				TerrainAdjustment.NONE
+			),
+			UniformInt.of(10, 20), BlockStateProvider.simple(TFBlocks.TWILIGHT_OAK_LOG.get()), TFLootTables.TREE_CACHE, TFEntities.SWARM_SPIDER
+		);
+	}
+}
