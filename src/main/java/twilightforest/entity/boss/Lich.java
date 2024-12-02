cf45063e8eb6476905858a294be02edc181ff7c7
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-12-02 11:05:18 -0700

    pick other targets

--- src/main/java/twilightforest/entity/boss/Lich.java
+++ src/main/java/twilightforest/entity/boss/Lich.java
@@ -644,6 +644,10 @@ public class Lich extends BaseTFBoss {
 		this.jumping = false;
 		//extinguish when teleporting
 		this.clearFire();
+		//set null target after teleporting so we choose a new target (for multiplayer fights)
+		if (this.getTarget() instanceof Player) {
+			this.setTarget(null);
+		}
 	}
 
 	//-----------------------------------------//
